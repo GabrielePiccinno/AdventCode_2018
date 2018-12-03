cb0ce9a764766c87b15da281634523b754d6e439
gabriele.piccinno <gabriele.piccinno@workday.com>
2018-12-03 10:22:36 +0000

    Day 03 Init

---
+++ src/main/com/workday/advent/code/Day03.java
@@ -0,0 +1,12 @@
+package com.workday.advent.code;
+
+import com.workday.advent.code.utils.DayUtils;
+
+import java.io.IOException;
+
+public class Day03 extends DayUtils {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
