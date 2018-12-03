541a3d23d438d7fa3dce594eccd63ab5932236eb
gabriele.piccinno <gabriele.piccinno@workday.com>
2018-12-03 16:50:41 +0000

    Day 03 Part 1 solution

--- src/main/com/workday/advent/code/Day03.java
+++ src/main/com/workday/advent/code/Day03.java
@@ -3,10 +3,62 @@ package com.workday.advent.code;
 import com.workday.advent.code.utils.DayUtils;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Day03 extends DayUtils {
+    int[][] matrix = new int[1000][1000];
+    int overlap = 0;
+
+    public class Claim {
+        int x;
+        int y;
+        int width;
+        int height;
+
+        @Override
+        public String toString() {
+            return String.format("X: %s; Y: %s; width: %s; height: %s", x, y, width, height);
+        }
+    }
+
+    public Claim parseInput(String input) {
+        if (input == null)
+            return null;
+        Claim c = new Claim();
+        String[] split = input.replaceAll("\\D+", " ").split(" ");
+        c.x = Integer.valueOf(split[2]);
+        c.y = Integer.valueOf(split[3]);
+        c.width = Integer.valueOf(split[4]);
+        c.height = Integer.valueOf(split[5]);
+        return c;
+
+    }
+
+    public void evaluateOverlap(Claim claim) {
+        System.out.println(claim);
+        for (int i = claim.x; i < claim.x+claim.width; i++) {
+            for (int j = claim.y; j < claim.y+claim.height; j++) {
+                matrix[i][j]++;
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
+        Day03 day03 = new Day03();
+        List<String> input = day03.readFileAsList("d03_input.txt");
+        for (String text : input) {
+            Claim claim = day03.parseInput(text);
+            day03.evaluateOverlap(claim);
+        }
 
+        for (int i = 0; i < day03.matrix.length; i++) {
+            for (int j = 0; j < day03.matrix[i].length; j++) {
+                if (day03.matrix[i][j] > 1) {
+                    day03.overlap++;
+                }
+            }
+        }
+
+        System.out.println(day03.overlap);
     }
 }
--- src/test/com/workday/advent/code/Day03Test.java
+++ src/test/com/workday/advent/code/Day03Test.java
@@ -1,33 +1,30 @@
 package com.workday.advent.code;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class Day02Test {
-    Day02 day02;
+class Day03Test {
+    Day03 day03;
 
     @BeforeEach
     void setUp() {
-        day02 = new Day02();
+        day03 = new Day03();
     }
 
-    @Test
-    void count() {
-        day02.countRepetitions("puppaaamozzo");
-        day02.printStatus();
-        day02.printChecksum();
-        Assertions.assertTrue(day02.twice == 2);
-        Assertions.assertTrue(day02.thrice == 2);
-    }
 
     @Test
-    void difference() {
-        String f1 = "ziaboiaa";
-        String f2 = "ziatroia";
-        day02.difference(f1, f2);
+    void parser() {
+        String c1 = "#1 @ 1,3: 4x4";
+        Day03.Claim claim = day03.parseInput(c1);
+        System.out.println(claim);
 
-        f2 = "ziaboiab";
-        day02.difference(f1, f2);
+        c1 = "#3 @ 5,5: 2x2";
+        claim = day03.parseInput(c1);
+        System.out.println(claim);
+
+        c1 = "#406 @ 619,507: 10x27";
+        claim = day03.parseInput(c1);
+        System.out.println(claim);
+
     }
 }
