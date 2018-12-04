4b57033bb36873a1ab86781388613bb70c938212
gabriele.piccinno <gabriele.piccinno@workday.com>
2018-12-04 18:15:43 +0000

    Day 04 part 1 init

--- src/main/com/workday/advent/code/Day04.java
+++ src/main/com/workday/advent/code/Day04.java
@@ -1,4 +1,46 @@
 package com.workday.advent.code;
 
-public class Day04 {
+import com.workday.advent.code.utils.DayUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Day04 extends DayUtils {
+
+    public class Record {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd HH:mm");
+        Date time;
+        int guardId;
+        boolean isAsleep;
+        boolean isAwake;
+
+        public Record(String input) throws ParseException {
+            parseInput(input);
+        }
+
+        public void parseInput(String input) throws ParseException {
+            time = format.parse(input.substring(input.indexOf("[")+1, input.indexOf("]")));
+            if (input.contains("Guard #")) {
+                String id = input.substring(input.indexOf("#")+1, input.indexOf(" begins"));
+                if (id != null) {
+                    guardId = Integer.valueOf(id);
+                }
+            }
+            isAsleep = input.contains("asleep");
+            isAwake = input.contains("wakes");
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Date: %s; IDGuard: %s; isAsleep: %s; isAwake: %s", time, guardId, isAsleep, isAwake);
+        }
+    }
+
+    public Record parseInput(String input) throws ParseException {
+        return new Record(input);
+    }
+
+
+
 }
--- src/test/com/workday/advent/code/Day04Test.java
+++ src/test/com/workday/advent/code/Day04Test.java
@@ -3,28 +3,30 @@ package com.workday.advent.code;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class Day03Test {
-    Day03 day03;
+import java.text.ParseException;
+
+class Day04Test {
+    Day04 day04;
 
     @BeforeEach
     void setUp() {
-        day03 = new Day03();
+        day04 = new Day04();
     }
 
 
     @Test
-    void parser() {
-        String c1 = "#1 @ 1,3: 4x4";
-        Day03.Claim claim = day03.parseInput(c1);
-        System.out.println(claim);
+    void parser() throws ParseException {
+        String c1 = "[1518-10-03 00:47] falls asleep";
+        Day04.Record record = day04.parseInput(c1);
+        System.out.println(record);
 
-        c1 = "#3 @ 5,5: 2x2";
-        claim = day03.parseInput(c1);
-        System.out.println(claim);
+        c1 = "[1518-06-22 00:48] wakes up";
+        record = day04.parseInput(c1);
+        System.out.println(record);
 
-        c1 = "#406 @ 619,507: 10x27";
-        claim = day03.parseInput(c1);
-        System.out.println(claim);
+        c1 = "[1518-07-26 23:50] Guard #487 begins shift";
+        record = day04.parseInput(c1);
+        System.out.println(record);
 
     }
 }
